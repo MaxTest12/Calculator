295fe322e2699f9969442f0949314e1e25070cac
MaxTest12 <110595966+MaxTest12@users.noreply.github.com>
2022-08-04 12:08:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Calculator 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // first number 
+            Console.WriteLine("Wpisz pierwszą liczbę:");
+              float num_1 = Convert.ToSingle(Console.ReadLine());
+            // operator 
+            Console.WriteLine("Wpisz Operator:");
+              string operators = Console.ReadLine();
+            // second number
+            Console.WriteLine("Wpisz drugą liczbę:");
+              float num_2 = Convert.ToSingle(Console.ReadLine());
+            // Sum
+            if(operators == "+")
+            {
+              Console.WriteLine(num_1+num_2);  
+            }   
+            // substraction 
+            else if(operators == "-")
+            {
+              Console.WriteLine(num_1 - num_2);
+            }    
+            // multiplication
+            else if(operators == "*")
+            {
+              Console.WriteLine(num_1 * num_2);
+            }
+            // divide 
+            else if(operators == ":")
+            {
+              Console.WriteLine(num_1 / num_2);
+            }
+            //Modulo
+            else if(operators == "%")
+            {
+              Console.WriteLine(num_1 % num_2);
+            }
+            else 
+            {
+              Console.WriteLine("nie ma takiego operatora");
+            }
+        }
+    }
+}
