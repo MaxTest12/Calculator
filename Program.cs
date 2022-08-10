b28b877151b8ffdbb6f9537b45c8df8379684576
MaxTest12 <110595966+MaxTest12@users.noreply.github.com>
2022-08-10 20:35:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,39 +9,33 @@
               float num_1 = Convert.ToSingle(Console.ReadLine());
             // operator 
             Console.WriteLine("Wpisz Operator:");
-              string operators = Console.ReadLine();
+            Console.WriteLine("1 - Dodawanie");
+            Console.WriteLine("2 - Odejmowanie");
+            Console.WriteLine("3 - Mnożenie");
+            Console.WriteLine("4 - Dzielenie");
+            Console.WriteLine("5 - Dzielenie z resztą");
+              int operators = Convert.ToInt32(Console.ReadLine());
             // second number
             Console.WriteLine("Wpisz drugą liczbę:");
               float num_2 = Convert.ToSingle(Console.ReadLine());
-            // Sum
-            if(operators == "+")
-            {
-              Console.WriteLine(num_1+num_2);  
-            }   
-            // substraction 
-            else if(operators == "-")
-            {
+          switch(operators)
+          {
+            case 1:
+              Console.WriteLine(num_1 + num_2); 
+              break;
+            case 2:
               Console.WriteLine(num_1 - num_2);
-            }    
-            // multiplication
-            else if(operators == "*")
-            {
+              break;
+            case 3:
               Console.WriteLine(num_1 * num_2);
-            }
-            // divide 
-            else if(operators == ":")
-            {
+              break;
+            case 4:
               Console.WriteLine(num_1 / num_2);
-            }
-            //Modulo
-            else if(operators == "%")
-            {
+              break;
+            case 5:
               Console.WriteLine(num_1 % num_2);
-            }
-            else 
-            {
-              Console.WriteLine("nie ma takiego operatora");
-            }
+              break;
+          }
         }
     }
 }
